1e5a7c8ed69ef9b27aa34a8f1d0cbf290c76fcd8
bruce <474949929@qq.com>
2014-02-28 14:55:32 +0800

    Update scrolltopcontrol.js

--- media/js/scrolltopcontrol.js
+++ media/js/scrolltopcontrol.js
@@ -44,6 +44,7 @@ var scrolltotop={
 		}
 	},init:function(){
 		jQuery(document).ready(function($){
+			debugger;
 			var mainobj=scrolltotop;
 			var iebrws=document.all;
 			mainobj.cssfixedsupport=!iebrws||iebrws&&document.compatMode=="CSS1Compat"&&window.XMLHttpRequest;
