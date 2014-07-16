2cb6b0cc8d27c1032844f9d76b7210091e520d03
bruce <474949929@qq.com>
2014-07-16 17:40:32 +0800

    11

--- js/post.js
+++ js/post.js
@@ -23,19 +23,7 @@
 
     $('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
 
-    //***********************
-    //**评论的代码也删掉哦***
-    alert(1)
-   window.disqus_shortname = 'wayne-lu'; // required: replace example with your forum shortname
-    $('#disqus_container .comment').on('click',function(){
-        $(this).html('加载中...');
-       var that = this;
-        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
-   });
-    //**评论的代码也删掉哦***
-    //***********************
-alert(1)
-    $('.entry a').each(function(index,element){
+       $('.entry a').each(function(index,element){
         var href = $(this).attr('href');
         if(href){
             if(href.indexOf('#') == 0){
